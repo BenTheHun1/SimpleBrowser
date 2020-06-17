6c25c2454038ca960ec2d57155631205c810059a
Benjamin Hunsicker <benhunsicker1@gmail.com>
2020-06-16 20:34:52 -0400

    Initial commit

---
+++ SimpleBrowser/Form1.Designer.vb
@@ -0,0 +1,130 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmWeb
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.txtURL = New System.Windows.Forms.TextBox()
+        Me.btnEnter = New System.Windows.Forms.Button()
+        Me.btnRefresh = New System.Windows.Forms.Button()
+        Me.btnForward = New System.Windows.Forms.Button()
+        Me.btnBack = New System.Windows.Forms.Button()
+        Me.webBrowser = New System.Windows.Forms.WebBrowser()
+        Me.GroupBox1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.txtURL)
+        Me.GroupBox1.Controls.Add(Me.btnEnter)
+        Me.GroupBox1.Controls.Add(Me.btnRefresh)
+        Me.GroupBox1.Controls.Add(Me.btnForward)
+        Me.GroupBox1.Controls.Add(Me.btnBack)
+        Me.GroupBox1.Dock = System.Windows.Forms.DockStyle.Top
+        Me.GroupBox1.Location = New System.Drawing.Point(0, 0)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(800, 44)
+        Me.GroupBox1.TabIndex = 0
+        Me.GroupBox1.TabStop = False
+        '
+        'txtURL
+        '
+        Me.txtURL.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.txtURL.Location = New System.Drawing.Point(114, 14)
+        Me.txtURL.Name = "txtURL"
+        Me.txtURL.Size = New System.Drawing.Size(617, 20)
+        Me.txtURL.TabIndex = 1
+        '
+        'btnEnter
+        '
+        Me.btnEnter.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.btnEnter.Location = New System.Drawing.Point(737, 12)
+        Me.btnEnter.Name = "btnEnter"
+        Me.btnEnter.Size = New System.Drawing.Size(57, 22)
+        Me.btnEnter.TabIndex = 2
+        Me.btnEnter.Text = "=>"
+        Me.btnEnter.UseVisualStyleBackColor = True
+        '
+        'btnRefresh
+        '
+        Me.btnRefresh.Location = New System.Drawing.Point(79, 12)
+        Me.btnRefresh.Name = "btnRefresh"
+        Me.btnRefresh.Size = New System.Drawing.Size(29, 22)
+        Me.btnRefresh.TabIndex = 5
+        Me.btnRefresh.Text = "@"
+        Me.btnRefresh.UseVisualStyleBackColor = True
+        '
+        'btnForward
+        '
+        Me.btnForward.Enabled = False
+        Me.btnForward.Location = New System.Drawing.Point(44, 12)
+        Me.btnForward.Name = "btnForward"
+        Me.btnForward.Size = New System.Drawing.Size(29, 22)
+        Me.btnForward.TabIndex = 4
+        Me.btnForward.Text = ">"
+        Me.btnForward.UseVisualStyleBackColor = True
+        '
+        'btnBack
+        '
+        Me.btnBack.Enabled = False
+        Me.btnBack.Location = New System.Drawing.Point(9, 12)
+        Me.btnBack.Name = "btnBack"
+        Me.btnBack.Size = New System.Drawing.Size(29, 22)
+        Me.btnBack.TabIndex = 3
+        Me.btnBack.Text = "<"
+        Me.btnBack.UseVisualStyleBackColor = True
+        '
+        'webBrowser
+        '
+        Me.webBrowser.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.webBrowser.Location = New System.Drawing.Point(0, 44)
+        Me.webBrowser.MinimumSize = New System.Drawing.Size(20, 20)
+        Me.webBrowser.Name = "webBrowser"
+        Me.webBrowser.Size = New System.Drawing.Size(800, 406)
+        Me.webBrowser.TabIndex = 6
+        Me.webBrowser.Url = New System.Uri("http://google.com", System.UriKind.Absolute)
+        '
+        'frmWeb
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.AutoScroll = True
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.webBrowser)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Name = "frmWeb"
+        Me.Text = "Simple Browser"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents txtURL As TextBox
+    Friend WithEvents btnEnter As Button
+    Friend WithEvents btnForward As Button
+    Friend WithEvents btnBack As Button
+    Friend WithEvents webBrowser As WebBrowser
+    Friend WithEvents btnRefresh As Button
+End Class
---
+++ SimpleBrowser/Form1.vb
@@ -0,0 +1,36 @@
+﻿Public Class frmWeb
+    Private Sub frmWeb_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        webBrowser.ScriptErrorsSuppressed = True
+        webBrowser.Refresh()
+    End Sub
+
+    Private Sub WebBrowser1_DocumentCompleted(sender As Object, e As WebBrowserDocumentCompletedEventArgs) Handles webBrowser.DocumentCompleted
+        txtURL.Text = webBrowser.Document.Url.AbsoluteUri
+        If webBrowser.CanGoForward = True Then
+            btnForward.Enabled = True
+        Else
+            btnForward.Enabled = False
+        End If
+        If webBrowser.CanGoBack = True Then
+            btnBack.Enabled = True
+        Else
+            btnBack.Enabled = False
+        End If
+    End Sub
+
+    Private Sub btnBack_Click(sender As Object, e As EventArgs) Handles btnBack.Click
+        webBrowser.GoBack()
+    End Sub
+
+    Private Sub btnForward_Click(sender As Object, e As EventArgs) Handles btnForward.Click
+        webBrowser.GoForward()
+    End Sub
+
+    Private Sub btnEnter_Click(sender As Object, e As EventArgs) Handles btnEnter.Click
+        webBrowser.Navigate(txtURL.Text)
+    End Sub
+
+    Private Sub btnRefresh_Click(sender As Object, e As EventArgs) Handles btnRefresh.Click
+        webBrowser.Refresh()
+    End Sub
+End Class
